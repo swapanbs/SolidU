1cb7c4f95c5784b683a95e19f7bb2c99acae8984
swapanbs <swapan@bs-23.net>
2021-02-22 20:25:58 +0600

    OutLine

---
+++ Assets/Solid/Scripts/OutLineFindingResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutLineFindingResponse : MonoBehaviour,IFindingResponse
+{
+    public void OnFind(Transform selection)
+    {
+        var selectionRenderer = selection.GetComponent<Outline>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.OutlineWidth=10;
+        }
+    }
+
+    public void OnLose(Transform selection)
+    {
+        var selectionRenderer = selection.GetComponent<Outline>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.OutlineWidth = 0;
+
+        }
+    }
+
+}
